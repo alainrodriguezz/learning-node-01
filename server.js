6b155225899b2c054a1710e00da6b8028c08198e
alain <alainrodriguezz@gmail.com>
2018-05-27 21:22:29 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+
+hbs.registerPartials(__dirname+'/views/partials')
+hbs.registerHelper('getYear',()=>{
+	return new Date().getFullYear()
+})
+hbs.registerHelper('scream',(text)=>{
+	return text.toUpperCase()
+})
+
+app.set('view engine','hbs')
+
+app.use((req,res,next)=>{
+	let now = new Date().toString()
+	let log = `${now} : ${req.method} ${req.url}` 
+	fs.appendFile('server.log',log + '\n')
+	next()
+})
+
+/*
+app.use((req,res,next)=>{
+	res.render('maintenance.hbs')
+})*/
+
+app.use(express.static(`${__dirname}/public`))
+
+
+app.get('/',(req,res)=>{
+	res.render('home.hbs',{
+		title:'Welcome Home'
+	})
+})
+
+app.get('/about',(req,res)=>{
+	res.render('about.hbs',{
+		title:'About Title'
+	})	
+})
+
+app.listen(3002,()=>{
+	console.log("Server Running on 3002")
+})
